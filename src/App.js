5f568e1cd51f60691e709be2be22c955bcd017a2
arsalyou <arsalyou@users.noreply.github.com>
2020-12-10 20:26:44 +0500

    Chat Reducer Added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import {useSelector} from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import Messenger from './component/Messenger'
-import {selectUser} from './features/userSlice'
+import { selectUser, login, logout } from './features/userSlice'
 import Login from './component/Login';
+import { auth } from './firebase';
 
 function App() {
 
   const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
-  
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      if (authUser) {
+        dispatch(login({
+          uid: authUser.uid,
+          email: authUser.email,
+          displayName: authUser.displayName,
+          photo: authUser.photoURL
+         })
+        );
+
+      } else {
+        dispatch(logout());
+
+      }
+    })
+
+  }, [])
 
   return (
     <div className="App">
-     {user ? <Messenger/> : <Login></Login>}
+      {user ? <Messenger /> : <Login></Login>}
     </div>
   );
 }
--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -1,38 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Sidebar.css"
 import { Avatar, Button, IconButton } from "@material-ui/core"
 import SearchIcon from '@material-ui/icons/Search';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import SidebarChat from './SidebarChat';
+import { useSelector } from "react-redux";
+import { selectUser } from '../features/userSlice'
+import db, { auth } from '../firebase'
 
 function Sidebar(props) {
-    return (
-        <div className="sidebar">
-            <div className="sidebar_header">
-                <Avatar className="sidebar_avatar">N</Avatar>
-                <div className="search_area">
-                    <SearchIcon></SearchIcon>
-                    <input></input>
-                </div>
-                <AddCircleIcon />
+    const user = useSelector(selectUser);
+    const [chats, setChats] = useState([]);
 
-            </div>
-            <div className="sidebar_chats">
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
-                <SidebarChat/>
+    useEffect(() => {
+        db.collection('chats').onSnapshot(snapshot => (
+            setChats(snapshot.docs.map((doc) => ({
+                id: doc.id,
+                data: doc.data(),
+            })
+            ))
+        ))
+}, [])
 
+    const addChat = ()=>{
+
+        const name = prompt("ENTER CHAT Name");
+        if(name){
+        db.collection('chats').add({
+            chatName: name,
+        });
+        }
+
+    }
+
+return (
+    <div className="sidebar">
+        <div className="sidebar_header">
+            <Avatar src={user.photo} onClick={() => auth.signOut()} className="sidebar_avatar">N</Avatar>
+            <div className="search_area">
+                <SearchIcon></SearchIcon>
+                <input></input>
             </div>
+            <AddCircleIcon onClick={addChat} />
+
         </div>
-    );
+        <div className="sidebar_chats">
+            {chats.map(({id, data:{chatName}}) => (
+                 <SidebarChat key={id} id={id} chatName={chatName} />
+            ))}
+        </div>
+    </div>
+);
 }
 
 export default Sidebar;
